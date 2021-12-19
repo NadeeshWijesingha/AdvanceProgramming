7a1d985c3452d4f98532cf3e5163e3d75add273b
Nadeesh <nadeeshwijesingha@gmail.com>
2021-12-19 16:31:52 +0530

    Introduce Explaining Variable

--- src/Location.java
+++ src/Location.java
@@ -1,26 +1,26 @@
 
 public class Location extends SpacePlace {
-  public int c;
-  public int r;
+  public int column;
+  public int row;
   public DIRECTION d;
   
   public enum DIRECTION {VERTICAL, HORIZONTAL};
   
-  public Location(int r, int c) {
-    this.r = r;
-    this.c = c;
+  public Location(int r, int column) {
+    this.row = r;
+    this.column = column;
   }
 
-  public Location(int r, int c, DIRECTION d) {    
-    this(r,c);
+  public Location(int r, int column, DIRECTION d) {
+    this(r, column);
     this.d=d;
   }
   
   public String toString() {
     if(d==null){
-      return "(" + (c+1) + "," + (r+1) + ")";
+      return "(" + (column +1) + "," + (row +1) + ")";
     } else {
-      return "(" + (c+1) + "," + (r+1) + "," + d + ")";
+      return "(" + (column +1) + "," + (row +1) + "," + d + ")";
     }
   }
 }
